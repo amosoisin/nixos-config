f778e6c99c85da68c74b3bb87d8ec472bb935563
amosoisin <23148082+amosoisin@users.noreply.github.com>
2026-01-13 23:51:36 +0900

    nixosユーザーの設定を追加

--- configuration.nix
+++ configuration.nix
@@ -41,11 +41,12 @@
     ohMyZsh.enable = true;
   };
 
-  users.users.dev = {
+  users.users.nixos = {
     isNormalUser = true;
     extraGroups = [ "docker" ];
     shell = pkgs.zsh;
   };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
