6105cfb81eb487e7daa39e379862f647b99d545a
amosoisin <23148082+amosoisin@users.noreply.github.com>
2026-01-13 23:35:57 +0900

    oh-my-zshの設定を追加

--- home.nix
+++ home.nix
@@ -11,4 +11,29 @@
 
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "docker" "kubectl" ];
+    };
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+
+    initExtra = ''
+      # Powerlevel10k設定ファイルの読み込み
+      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+    '';
+  };
 }
