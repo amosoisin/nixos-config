280d1d99cb75709fdf98d301bae05f03ae6e3bd0
amosoisin <23148082+amosoisin@users.noreply.github.com>
2026-01-14 20:24:18 +0900

    zsh設定を分離

--- home.nix
+++ home.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ...}:
 
 {
+  imports = [
+    ./zsh.nix
+  ];
+
   home.username = "nixos";
   home.homeDirectory = "/home/nixos";
 
@@ -162,55 +166,5 @@
       # ウィンドウを閉じた時に番号を詰める
       set -g renumber-windows on
     '';
-  };
-
-  # ===== Zsh設定 =====
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "docker"
-        "fzf"
-        "zoxide"
-        "golang"
-        "rust"
-        "npm"
-        "pip"
-      ];
-    };
-
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "you-should-use";
-        src = pkgs.zsh-you-should-use;
-        file = "share/zsh/plugins/you-should-use/you-should-use.plugin.zsh";
-      }
-      {
-        name = "fzf-tab";
-        src = pkgs.zsh-fzf-tab;
-        file = "share/fzf-tab/fzf-tab.plugin.zsh";
-      }
-      {
-        name = "forgit";
-        src = pkgs.zsh-forgit;
-        file = "share/zsh/zsh-forgit/forgit.plugin.zsh";
-      }
-      {
-        name = "my-config";
-        src = ./config/zsh;
-        file = "my-config.plugin.zsh";
-      }
-    ];
   };
 }
---
+++ zsh.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+{
+  # ===== Zsh設定 =====
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "docker"
+        "fzf"
+        "zoxide"
+        "golang"
+        "rust"
+        "npm"
+        "pip"
+      ];
+    };
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "you-should-use";
+        src = pkgs.zsh-you-should-use;
+        file = "share/zsh/plugins/you-should-use/you-should-use.plugin.zsh";
+      }
+      {
+        name = "fzf-tab";
+        src = pkgs.zsh-fzf-tab;
+        file = "share/fzf-tab/fzf-tab.plugin.zsh";
+      }
+      {
+        name = "forgit";
+        src = pkgs.zsh-forgit;
+        file = "share/zsh/zsh-forgit/forgit.plugin.zsh";
+      }
+      {
+        name = "my-config";
+        src = ./config/zsh;
+        file = "my-config.plugin.zsh";
+      }
+    ];
+  };
+}
