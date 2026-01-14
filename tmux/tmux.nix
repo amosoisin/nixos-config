0af109b92faaa91ace39da4d4edb13bfe303deb7
amosoisin <23148082+amosoisin@users.noreply.github.com>
2026-01-14 22:48:49 +0900

    tmuxの設定を更新

--- tmux/tmux.nix
+++ tmux/tmux.nix
@@ -5,44 +5,48 @@
   programs.tmux = {
     enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
+    shortcut = "b";
     terminal = "tmux-256color";
+    keyMode = "vi";
+    baseIndex = 1;
+    mouse = true;
+    escapeTime = 0;
+    newSession = true;
+    historyLimit = 50000;
+
+    # Force tmux to use /tmp for sockets (WSL2 compat)
+    secureSocket = false;
+
+    # plugins
     plugins = with pkgs.tmuxPlugins; [
       sensible
       yank
       resurrect
       continuum
+      pain-control
+      {
+        plugin = dracula;
+        extraConfig = ''
+          set -g @dracula-plugins "git cwd"
+          set -g @dracula-cpu-usage-colors "pink dark_gray"
+          set -g @dracula-show-flags true
+          set -g @dracula-show-powerline true
+          set -g @dracula-git-show-remote true
+          set -g @dracula-git-show-dirty true
+        '';
+      }
     ];
     extraConfig = ''
-      # マウスサポート
-      set -g mouse on
+      # ===== 色設定 =====
+      set -g terminal-overrides 'xterm:colors=256'
+      set-option -ga terminal-overrides ',xterm-256color:Tc'
 
-      # プレフィックスキーをC-aに変更
-      unbind C-b
-      set -g prefix C-a
-      bind C-a send-prefix
-
-      # ペイン分割のキーバインド
-      bind | split-window -h -c "#{pane_current_path}"
-      bind - split-window -v -c "#{pane_current_path}"
-
-      # Vimスタイルのペイン移動
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
-
-      # ペインリサイズ
-      bind -r H resize-pane -L 5
-      bind -r J resize-pane -D 5
-      bind -r K resize-pane -U 5
-      bind -r L resize-pane -R 5
-
-      # ウィンドウ番号を1から開始
-      set -g base-index 1
-      setw -g pane-base-index 1
-
-      # ウィンドウを閉じた時に番号を詰める
-      set -g renumber-windows on
+      # ===== シンクロペイン =====
+      # トグル
+      bind S set-window-option synchronize-panes
+      # 開始・終了を別キーに登録
+      bind a set-window-option synchronize-panes on
+      bind b set-window-option synchronize-panes off
     '';
   };
 }
