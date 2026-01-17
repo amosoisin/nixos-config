c3cc5036049ef5f9e6f924cdb41bcfb977cadf24
amosoisin <23148082+amosoisin@users.noreply.github.com>
2026-01-18 02:24:25 +0900

    ghosttyを削除

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -7,7 +7,6 @@
     ./tmux
     (import ./neovim { inherit config pkgs lib inputs; })
     (import ./claude { inherit pkgs-unstable; })
-    (import ./ghostty { inherit pkgs-unstable; })
   ];
 
   home.packages = with pkgs; [
