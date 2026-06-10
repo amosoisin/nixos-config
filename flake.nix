2c788abd47595a5dc41249e577379a92d199ffe4
amosoisin <23148082+amosoisin@users.noreply.github.com>
2026-06-10 08:56:23 +0000

    flake.nixを追加

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/release-26.05";
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "26.05";
+            wsl.enable = true;
+          }
+        ];
+      };
+    };
+  };
+}
