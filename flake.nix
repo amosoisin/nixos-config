c7e054961760b4fd0da1283d13c819a25fbc8d09
amosoisin <23148082+amosoisin@users.noreply.github.com>
2026-06-11 11:28:56 +0900

    ネットワーク設定用ファイルを追加

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          ./network.nix
           nixos-wsl.nixosModules.default
           {
             system.stateVersion = "26.05";
