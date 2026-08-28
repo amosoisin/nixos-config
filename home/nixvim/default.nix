4755d5e65d535b48d6e6c03d032fa2669d057ea9
amosoisin <23148082+amosoisin@users.noreply.github.com>
2026-08-28 19:15:04 +0900

    nixvim: telescopeのdiagnosticsキーマップを追加

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -282,6 +282,9 @@
           "<leader>fh" = {
             action = "help_tags";
           };
+          "<leader>fd" = {
+            action = "diagnostics";
+          };
         };
       };
 
